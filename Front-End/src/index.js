2a103de854621a99974c2e3ca944d1d01b063071
Danny Paez <dpaez16@yahoo.com>
2020-05-29 17:53:33 -0500

    removed unused libraries

--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css'
-import 'katex/dist/katex.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
