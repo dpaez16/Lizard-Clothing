e1983f86160436e4929331a195ca07542cdd9ac0
Danny Paez <dpaez16@yahoo.com>
2020-09-10 17:29:07 -0700

    db_service.go has started

---
+++ Back-End/db_service.go
@@ -0,0 +1,57 @@
+package main
+
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+//	"go.mongodb.org/mongo-driver/bson"
+//	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"fmt"
+	"log"
+	"os"
+	"context"
+	"time"
+)
+
+
+var DB_USER = os.Getenv("DB_USER")
+var DB_PASSWORD = os.Getenv("DB_PASSWORD")
+var DB_NAME = "lizardClothingDB"
+var COLLECTION_NAME = "catalog"
+
+//type Product struct {}
+
+
+func getContext(nSeconds int) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithTimeout(context.Background(), nSeconds * time.Second)
+	return ctx, cancel
+}
+
+
+func getDBClient() (*mongo.Client, error) {
+	ctx, cancel := getContext(10)
+	defer cancel()
+
+	url := fmt.Sprintf(`
+		mongodb+srv://%s:%s@lizard-clothing
+		.i89ud.mongodb.net/%s?retryWrites=
+		true&w=majority`, DB_USER, DB_PASSWORD, DB_NAME)
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(url,))
+	return client, err
+}
+
+
+//func insertProduct() () {}
+//func queryCatalog() () {}
+
+
+func main() {
+	client, err := getDBClient()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection := client.Database(DB_NAME).Collection(COLLECTION_NAME)
+}
